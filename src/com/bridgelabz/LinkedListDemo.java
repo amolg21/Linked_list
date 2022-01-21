a941366c8c076beeb43d675938489a3fdd949ccf
Amol Ghotale <amol9.ag@gmail.com>
2022-01-21 11:36:29 +0530

    ADDED: Link list created

---
+++ src/com/bridgelabz/LinkedListDemo.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+class Node {
+
+    int data;
+    Node next;
+
+    //Parameterized Constructor to initialize Data
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
